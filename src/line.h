b500c46be3e82cffa38b5c420573873de1fb1f4f
masnnuller <mas.null19@gmail.com>
2012-06-04 19:37:28 +0900

    add function to line.cpp and line.h

--- src/line.h
+++ src/line.h
@@ -3,15 +3,20 @@
 #include "ofxTuio.h"
 class line{
     public:
-    line();
+    line(){};
     line(ofxTuioObject * _object,ofxTuioCursor * _cursor);
-    ~line();
+    ~line(){};
     void draw();
     void update(ofxTuioCursor * _cursor);
     void update(ofxTuioObject * _object);
+    void update(ofxTuioObject * _object, ofxTuioCursor * _cursor);
+    void lineEnd(ofxTuioObject * _object);
+    int getfromID();
+    int getToID();
+    int getCursorID();
     private:
     ofxTuioObject from,to;
     ofxTuioCursor tp;
-    bool isEnd
+    bool isEnd;
 };
 #endif // LINE_H_INCLUDED
